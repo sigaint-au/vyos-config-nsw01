cdd7be9cc88a739416d12956377517be7983027e
mhahl <mark.hahl@sigaint.au>
2025-01-07 18:38:54 +1100

    add configs

---
+++ config-parts/interfaces.sh
@@ -0,0 +1,49 @@
+#/bin/vbash
+
+# Set MAC Addresses
+set interfaces ethernet eth0 hw-id '00:0d:b9:5e:41:34'
+set interfaces ethernet eth1 hw-id '00:0d:b9:5e:41:35'
+set interfaces ethernet eth2 hw-id '00:0d:b9:5e:41:36'
+set interfaces ethernet eth3 hw-id '00:0d:b9:5e:41:37'
+
+# Interfaces for WAN
+set interfaces ethernet eth0 address 'dhcp'
+set interfaces ethernet eth0 address 'dhcpv6'
+set interfaces ethernet eth0 description 'NET_NSW01_WAN'
+set interfaces ethernet eth0 dhcpv6-options pd 0 interface br0.13 address '1'
+set interfaces ethernet eth0 dhcpv6-options pd 0 interface br0.13 sla-id '0'
+set interfaces ethernet eth0 dhcpv6-options pd 0 interface br0.20 address '1'
+set interfaces ethernet eth0 dhcpv6-options pd 0 interface br0.20 sla-id '1'
+set interfaces ethernet eth0 dhcpv6-options pd 0 length '48'
+set interfaces ethernet eth0 dhcpv6-options rapid-commit
+set interfaces ethernet eth0 ipv6 address autoconf
+
+# Configure bridge interface and VLANs.
+set interfaces bridge br0 enable-vlan
+set interfaces bridge br0 member interface eth3 allowed-vlan '10-50'
+set interfaces bridge br0 member interface eth4 allowed-vlan '10-50'
+set interfaces bridge br0 stp
+set interfaces bridge br0 vif 10 address '10.120.10.1/24'
+set interfaces bridge br0 vif 10 description 'NET_NSW01_ADMIN'
+set interfaces bridge br0 vif 11 address '10.120.11.1/27'
+set interfaces bridge br0 vif 11 description 'NET_NSW01_PROTECTED'
+set interfaces bridge br0 vif 12 address '10.120.12.1/24'
+set interfaces bridge br0 vif 12 description 'NET_NSW01_SECURITY'
+set interfaces bridge br0 vif 13 address '10.120.13.1/24'
+set interfaces bridge br0 vif 13 description 'NET_NSW01_WIFI'
+set interfaces bridge br0 vif 14 address '10.120.14.1/24'
+set interfaces bridge br0 vif 14 description 'NET_NSW01_HOSTING'
+set interfaces bridge br0 vif 20 address '10.120.20.1/24'
+set interfaces bridge br0 vif 20 description 'NET_NSW01_DMZ'
+set interfaces bridge br0 vif 21 address '10.120.21.1/24'
+set interfaces bridge br0 vif 21 description 'NET_NSW01_VMNET'
+
+# WireGuard configuration for ProtonVPN CH600
+set interfaces wireguard wg600 address '10.2.0.2/32'
+set interfaces wireguard wg600 description 'ProtonVPN_CH600'
+set interfaces wireguard wg600 peer ProtonVPN_CH600 address '79.127.184.216'
+set interfaces wireguard wg600 peer ProtonVPN_CH600 allowed-ips '0.0.0.0/0'
+set interfaces wireguard wg600 peer ProtonVPN_CH600 persistent-keepalive '15'
+set interfaces wireguard wg600 peer ProtonVPN_CH600 port '51820'
+set interfaces wireguard wg600 peer ProtonVPN_CH600 public-key 'Ii6hAbnu84wZ8NzVt5+ylO4FnX+ANrKNzpFOSYq9dks='
+set interfaces wireguard wg600 private-key "$wireguard_wg600_private_key"
---
+++ config-parts/service_dhcp-server.sh
@@ -0,0 +1,67 @@
+#!/bin/vbash
+
+#----------------------------------------------------------
+# Name:     NET_NSW01_MGMT
+# Network:  10.120.10.1
+# VLAN:     10
+# Domain:   mgmt.nsw01.sigaint.au
+#
+#----------------------------------------------------------
+
+set service dhcp-server shared-network-name NET_NSW01_MGMT option domain-name 'mgmt.nsw01.sigaint.au'
+set service dhcp-server shared-network-name NET_NSW01_MGMT option name-server '10.120.10.1'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 option default-router '10.120.10.1'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 range 0 start '10.120.10.10'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 range 0 stop '10.120.10.100'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 subnet-id '1'
+
+## Static Mapping
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-6BFVY42 ip-address '10.120.10.10'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-6BFVY42 mac-address '74:e6:e2:fa:70:06'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-8505qm2 ip-address '10.120.10.11'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-8505qm2 mac-address '58:8a:5a:e7:4a:c2'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-9qk2vg2 ip-address '10.120.10.12'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-9qk2vg2 mac-address '84:7b:eb:d9:0d:c'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-9qd4vg2 ip-address '10.120.10.13'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping idrac-9qd4vg2 mac-address ' 84:7b:eb:d9:0d:bc'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping xcc-7x02-j300cw5m ip-address '10.120.10.14'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping xcc-7x02-j300cw5m mac-address '7c:d3:0a:59:3f:95'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet 10.120.10.0/24 static-mapping xcc-7x02-j300cw5k ip-address '10.120.10.15'
+set service dhcp-server shared-network-name NET_NSW01_MGMT subnet  10.120.10.0/24 static-mapping xcc-7x02-j300cw5k mac-address '38:68:dd:05:7e:dd'
+
+# DMZ VLAN
+set service dhcp-server shared-network-name NET_NSW01_DMZ option domain-name 'dmz.nsw01.sigaint.au'
+set service dhcp-server shared-network-name NET_NSW01_DMZ option name-server '10.120.20.1'
+set service dhcp-server shared-network-name NET_NSW01_DMZ subnet 10.120.20.0/24 option default-router '10.120.20.1'
+set service dhcp-server shared-network-name NET_NSW01_DMZ subnet 10.120.20.0/24 range 0 start '10.120.20.10'
+set service dhcp-server shared-network-name NET_NSW01_DMZ subnet 10.120.20.0/24 range 0 stop '10.120.20.100'
+set service dhcp-server shared-network-name NET_NSW01_DMZ subnet 10.120.20.0/24 subnet-id '3'
+
+# OpenShift Bare Metal VLAN
+set service dhcp-server shared-network-name NET_NSW01_HOSTING option domain-name 'ocp.nsw01.sigaint.au'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING option name-server '10.120.14.1'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet 10.120.14.0/24 option default-router '10.120.14.1'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet 10.120.14.0/24 range 0 start '10.120.14.100'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet 10.120.14.0/24 range 0 stop '10.120.14.200'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet 10.120.14.0/24 subnet-id '4'
+
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-01 ip-address '10.120.14.11'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-01 mac-address '44:a8:42:0a:5e:e7'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-02 ip-address '10.120.14.12'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-02 mac-address '80:18:44:ec:23:e6'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-03 ip-address '10.120.14.13'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-03 mac-address '18:66:da:ef:6d:8a'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-04 ip-address '10.120.14.14'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-04 mac-address '18:66:da:ef:7d:aa'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-05 ip-address '10.120.14.15'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-05 mac-address '7c:d3:0a:59:3f:90'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-06 ip-address '10.120.14.16'
+set service dhcp-server shared-network-name NET_NSW01_HOSTING subnet  10.120.14.0/24 static-mapping lan-node-06 mac-address '38:68:dd:05:7e:d8'
+
+# Security VLAN
+set service dhcp-server shared-network-name NET_NSW01_SECURITY option domain-name 'security.nsw01.sigaint.au'
+set service dhcp-server shared-network-name NET_NSW01_SECURITY option name-server '10.120.12.1'
+set service dhcp-server shared-network-name NET_NSW01_SECURITY subnet 10.120.12.0/24 option default-router '10.120.12.1'
+set service dhcp-server shared-network-name NET_NSW01_SECURITY subnet 10.120.12.0/24 range 0 start '10.120.12.10'
+set service dhcp-server shared-network-name NET_NSW01_SECURITY subnet 10.120.12.0/24 range 0 stop '10.120.12.100'
+set service dhcp-server shared-network-name NET_NSW01_SECURITY subnet 10.120.12.0/24 subnet-id '2'
