cb718add985df69a474714dae05e820c6d6547cb
mhahl <mark.hahl@sigaint.au>
2025-01-07 20:58:54 +1100

    add policy route

---
+++ config-parts/protocols.sh
@@ -0,0 +1,3 @@
+#/bin/vbash
+
+set protocols static table 10 route 0.0.0.0/0 interface wg600
--- config-parts/system.sh
+++ config-parts/system.sh
@@ -3,12 +3,19 @@
 # Hostname
 set system host-name 'lan-rou-01.nsw01.sigaint.au'
 
+set system time-zone 'Aystralia/Sydney'
+
 # System DNS set to NextDNS
 set system name-server '45.90.28.189'
 set system name-server '45.90.30.189'
 set system name-server '2a07:a8c0::2a:b561'
 set system name-server '2a07:a8c1::2a:b561'
 
-# Syslog
-set system syslog global facility all level 'info'
-set system syslog global facility local7 level 'debug'
+# Device requires this baud rate
+set system console device ttyS0 speed '115200'
+
+# Set welcome banner
+set system login banner post-login 'Welcome to VyOS!\n\n  ┌── ┐\n  . VyOS 1.5 \n  └ ──┘'
+
+# Default password for vyos user
+set system login user vyos authentication encrypted-password '$6$rounds=656000$/ihVNMXJyZ51jk.D$KR6ersssBUiHTY.ZT5N8DdbSnOKw0u1D/6EjV1nIY6m1G0MhpkdPwTE2ypEgdvwskBXo3PcofK0cQkUKdkAUF/'
