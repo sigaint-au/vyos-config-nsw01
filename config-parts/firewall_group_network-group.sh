25fac3655720299d8b38fb42c87ddd07dbe8f58e
mhahl <mark.hahl@sigaint.au>
2025-01-09 09:11:47 +1100

    simplify groups

--- config-parts/firewall_group_network-group.sh
+++ config-parts/firewall_group_network-group.sh
@@ -2,14 +2,7 @@
 
 ##
 # Networks local to NSW01
-#
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.10.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.11.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.12.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.13.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.14.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.20.0/24'
-set firewall group network-group NSW01_LOCAL_NETS network '10.120.21.0/24'
+set firewall group network-group NSW01_LOCAL_NETS network '10.120.0.0/16'
 
 # IPv6 Local nets
 set firewall group ipv6-network-group NSW01_LOCAL_NETS_6 network '2404:e80:4b6f::/48'
