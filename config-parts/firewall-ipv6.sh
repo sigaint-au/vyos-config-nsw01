c3f2dac78b89966909fb57c6d7654831909476e1
mhahl <mark.hahl@sigaint.au>
2025-01-08 19:16:25 +1100

    add v6 ra and fw

--- config-parts/firewall-ipv6.sh
+++ config-parts/firewall-ipv6.sh
@@ -26,3 +26,27 @@ set firewall ipv6 name WAN-LOCAL-6 rule 140 description 'DHCPv6 Replies'
 set firewall ipv6 name WAN-LOCAL-6 rule 140 destination port '546'
 set firewall ipv6 name WAN-LOCAL-6 rule 140 protocol 'udp'
 set firewall ipv6 name WAN-LOCAL-6 rule 140 source port '547'
+
+# WIFI -> LOCAL
+set firewall ipv6 name WIFI-LOCAL-6 default-action 'reject'
+set firewall ipv6 name WIFI-LOCAL-6 default-log
+set firewall ipv6 name WIFI-LOCAL-6 description 'Wifi to LOCAL IPv6'
+set firewall ipv6 name WIFI-LOCAL-6 rule 100 action 'accept'
+set firewall ipv6 name WIFI-LOCAL-6 rule 100 protocol 'icmpv6'
+set firewall ipv6 name WIFI-LOCAL-6 rule 110 action 'accept'
+set firewall ipv6 name WIFI-LOCAL-6 rule 110 description 'DHCPv6 Replies'
+set firewall ipv6 name WIFI-LOCAL-6 rule 110 destination port '546'
+set firewall ipv6 name WIFI-LOCAL-6 rule 110 protocol 'udp'
+set firewall ipv6 name WIFI-LOCAL-6 rule 110 source port '547'
+
+# LOCAL -> WIFI
+set firewall ipv6 name LOCAL-WIFI-6 default-action 'drop'
+set firewall ipv6 name LOCAL-WIFI-6 default-log
+set firewall ipv6 name LOCAL-WIFI-6 description 'LOCAL to WIFI IPv6'
+set firewall ipv6 name LOCAL-WIFI-6 rule 110 action 'accept'
+set firewall ipv6 name LOCAL-WIFI-6 rule 110 state 'established'
+set firewall ipv6 name LOCAL-WIFI-6 rule 110 state 'related'
+set firewall ipv6 name LOCAL-WIFI-6 rule 120 action 'drop'
+set firewall ipv6 name LOCAL-WIFI-6 rule 120 state 'invalid'
+set firewall ipv6 name LOCAL-WIFI-6 rule 130 action 'accept'
+set firewall ipv6 name LOCAL-WIFI-6 rule 130 protocol 'icmpv6'
