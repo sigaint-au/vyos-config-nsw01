b283a2670b8ca5028e1b6c8f64e3f6b69d57693e
mhahl <mark.hahl@sigaint.au>
2025-01-09 09:14:12 +1100

    fix port

--- config-parts/policy_route6.sh
+++ config-parts/policy_route6.sh
@@ -2,7 +2,7 @@
 
 # All traffic coming on in on vlan13 (wifi) 2404:e80:4b6f::1/64 should be routed to table 10
 # through the VPN tunnel.
-set policy route6 NET-WIFI-VPN-6 interface 'br0.11'
+set policy route6 NET-WIFI-VPN-6 interface 'br0.13'
 set policy route6 NET-WIFI-VPN-6 rule 5 description 'Bypass VPN for local networks'
 set policy route6 NET-WIFI-VPN-6 rule 5 destination group network-group 'NSW01_LOCAL_NETS_6'
 set policy route6 NET-WIFI-VPN-6 rule 5 set table 'main'
