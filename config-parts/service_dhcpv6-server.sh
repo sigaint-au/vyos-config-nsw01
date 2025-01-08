bc12ee3f33c32cdd6f58e1e93c6ba2f06584f050
mhahl <mark.hahl@sigaint.au>
2025-01-08 19:26:46 +1100

    fix prefix

--- config-parts/service_dhcpv6-server.sh
+++ config-parts/service_dhcpv6-server.sh
@@ -5,9 +5,9 @@ set service dhcpv6-server global-parameters name-server '2a07:a8c0::2a:b561'
 set service dhcpv6-server global-parameters name-server '2a07:a8c1::2a:b561'
 
 # DMZ
-set service dhcpv6-server shared-network-name NET_NSW01_DMZ_6 common-options domain-search 'dmz.nsw01.sigaint.au'
-set service dhcpv6-server shared-network-name NET_NSW01_DMZ_6 subnet 2404:e80:4b6f:1::1/64 subnet-id '20'
+set service dhcpv6-server shared-network-name NET_NSW01_DMZ_6 option domain-search 'dmz.nsw01.sigaint.au'
+set service dhcpv6-server shared-network-name NET_NSW01_DMZ_6 subnet 2404:e80:4b6f:1::/64 subnet-id '20'
 
 # WIFI
-set service dhcpv6-server shared-network-name NET_NSW01_WIFI_6 common-options domain-search 'wifi.nsw01.sigaint.au'
-set service dhcpv6-server shared-network-name NET_NSW01_WIFI_6 subnet 2404:e80:4b6f::1/64 subnet-id '13'
+set service dhcpv6-server shared-network-name NET_NSW01_WIFI_6 option domain-search 'wifi.nsw01.sigaint.au'
+set service dhcpv6-server shared-network-name NET_NSW01_WIFI_6 subnet 2404:e80:4b6f::/64 subnet-id '13'
--- config-parts/service_router-advert.sh
+++ config-parts/service_router-advert.sh
@@ -10,4 +10,4 @@ set service router-advert interface br0.13 prefix 2404:e80:4b6f::/64
 set service router-advert interface br0.20 name-server '2a07:a8c0::2a:b561'
 set service router-advert interface br0.20 name-server '2a07:a8c1::2a:b561'
 set service router-advert interface br0.20 other-config-flag
-set service router-advert interface br0.20 2404:e80:4b6f:1::/64
+set service router-advert interface br0.20 prefix 2404:e80:4b6f:1::/64
