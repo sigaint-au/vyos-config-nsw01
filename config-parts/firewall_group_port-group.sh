f58cc2fe670d16da068590d466e7e2c6c437e667
mhahl <mark.hahl@sigaint.au>
2025-01-08 18:29:29 +1100

    add ports

--- config-parts/firewall_group_port-group.sh
+++ config-parts/firewall_group_port-group.sh
@@ -4,9 +4,9 @@
 set firewall group port-group HTTP_PORTS port '80'
 set firewall group port-group HTTP_PORTS port '443'
 
-set firewall group port-group LOCAL_SERVICES '3128'
-set firewall group port-group LOCAL_SERVICES '123'
-set firewall group port-group LOCAL_SERVICES '53'
+set firewall group port-group LOCAL_SERVICES port '3128'
+set firewall group port-group LOCAL_SERVICES port '123'
+set firewall group port-group LOCAL_SERVICES port '53'
 
 # Ports for IPA Server
 set firewall group port-group IPA_PORTS port '80'
