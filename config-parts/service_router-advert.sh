58a2e5e00453fdf7a3793931c962b991fd761cee
mhahl <mark.hahl@sigaint.au>
2025-01-08 20:54:33 +1100

    fix prefix

--- config-parts/service_router-advert.sh
+++ config-parts/service_router-advert.sh
@@ -16,19 +16,19 @@ set service router-advert interface br0.21 prefix 2404:e80:4b6f:6::/64
 set service router-advert interface br0.10 name-server '2a07:a8c0::2a:b561'
 set service router-advert interface br0.10 name-server '2a07:a8c1::2a:b561'
 set service router-advert interface br0.10 other-config-flag
-set service router-advert interface br0.10 2404:e80:4b6f:2::/64
+set service router-advert interface br0.10 prefix 2404:e80:4b6f:2::/64
 
 # PROTECTED - RA
 set service router-advert interface br0.11 name-server '2a07:a8c0::2a:b561'
 set service router-advert interface br0.11 name-server '2a07:a8c1::2a:b561'
 set service router-advert interface br0.11 other-config-flag
-set service router-advert interface br0.11 2404:e80:4b6f:3::/64
+set service router-advert interface br0.11 prefix 2404:e80:4b6f:3::/64
 
 # SECURITY - RA
 set service router-advert interface br0.12 name-server '2a07:a8c0::2a:b561'
 set service router-advert interface br0.12 name-server '2a07:a8c1::2a:b561'
 set service router-advert interface br0.12 other-config-flag
-set service router-advert interface br0.12 2404:e80:4b6f:4::/64
+set service router-advert interface br0.12 prefix 2404:e80:4b6f:4::/64
 
 # WIFI - RA
 set service router-advert interface br0.13 name-server '2a07:a8c0::2a:b561'
@@ -40,4 +40,4 @@ set service router-advert interface br0.13 prefix 2404:e80:4b6f::/64
 set service router-advert interface br0.14 name-server '2a07:a8c0::2a:b561'
 set service router-advert interface br0.14 name-server '2a07:a8c1::2a:b561'
 set service router-advert interface br0.14 other-config-flag
-set service router-advert interface br0.14 2404:e80:4b6f:5::/64
+set service router-advert interface br0.14 prefix 2404:e80:4b6f:5::/64
