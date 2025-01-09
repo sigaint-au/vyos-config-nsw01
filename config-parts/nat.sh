879a03284e03b1b25ed9a91e6e669c51fd1bfe2d
mhahl <mark.hahl@sigaint.au>
2025-01-09 14:47:34 +1100

    remove nat

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -1,14 +1,18 @@
 #!/bin/vbash
 
-set nat source rule 100 outbound-interface name 'eth0'
-set nat source rule 100 source address '10.120.0.0/16'
-set nat source rule 100 translation address 'masquerade'
+## Define NAT rules
+## Currently disabled.
+## NAT rules are not needed for the current configuration.
+#
+#set nat source rule 100 outbound-interface name 'eth0'
+#set nat source rule 100 source address '10.120.0.0/16'
+#set nat source rule 100 translation address 'masquerade'
 
 # Force port 53 traffic to use local dns
-set nat destination rule 102 description 'Force DNS for Wifi'
-set nat destination rule 102 destination address '!10.120.13.1'
-set nat destination rule 102 destination port '53'
-set nat destination rule 102 inbound-interface name 'br0.13'
-set nat destination rule 102 protocol 'tcp_udp'
-set nat destination rule 102 translation address '10.120.13.1'
-set nat destination rule 102 translation port '53'
+#set nat destination rule 102 description 'Force DNS for Wifi'
+#set nat destination rule 102 destination address '!10.120.13.1'
+#set nat destination rule 102 destination port '53'
+#set nat destination rule 102 inbound-interface name 'br0.13'
+#set nat destination rule 102 protocol 'tcp_udp'
+#set nat destination rule 102 translation address '10.120.13.1'
+#set nat destination rule 102 translation port '53'
