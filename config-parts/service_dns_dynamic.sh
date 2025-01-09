dd0a9bec5c0d2c90dbc4306dae522754d24c96c6
mhahl <mark.hahl@sigaint.au>
2025-01-09 12:27:50 +1100

    remove dynamic dns

--- config-parts/service_dns_dynamic.sh
+++ config-parts/service_dns_dynamic.sh
@@ -1,9 +1,13 @@
 #!/bin/vbash
+###
+## This is currently disabled as it is not working.
+## It is left here as a reference for future work.
+###
 
-set service dns dynamic name service-cloudflare-eth0 address interface 'eth0'
-set service dns dynamic name service-cloudflare-eth0 host-name 'au-lan-rou-01.nsw01.sigaint.au'
-set service dns dynamic name service-cloudflare-eth0 ip-version 'ipv6'
-set service dns dynamic name service-cloudflare-eth0 password "$secret_dns_dynamic_cloudflare_password"
-set service dns dynamic name service-cloudflare-eth0 protocol 'cloudflare'
-set service dns dynamic name service-cloudflare-eth0 username 'mark@hahl.id.au'
-set service dns dynamic name service-cloudflare-eth0 zone 'sigaint.au'
+#set service dns dynamic name service-cloudflare-eth0 address interface 'eth0'
+#set service dns dynamic name service-cloudflare-eth0 host-name 'au-lan-rou-01.nsw01.sigaint.au'
+#set service dns dynamic name service-cloudflare-eth0 ip-version 'ipv6'
+#set service dns dynamic name service-cloudflare-eth0 password "$secret_dns_dynamic_cloudflare_password"
+#set service dns dynamic name service-cloudflare-eth0 protocol 'cloudflare'
+#set service dns dynamic name service-cloudflare-eth0 username 'mark@hahl.id.au'
+#set service dns dynamic name service-cloudflare-eth0 zone 'sigaint.au'
