261ad4874e08ecb9a808e1179b926ec6f851c409
Mark Hahl <mark.hahl@sigaint.au>
2025-04-04 21:14:48 +1100

    add policy route for vlan 14 to table 10

--- config-parts/policy_route.sh
+++ config-parts/policy_route.sh
@@ -5,10 +5,15 @@ set policy route NET-WIFI-VPN rule 5 description 'Bypass VPN for local networks'
 set policy route NET-WIFI-VPN rule 5 destination group network-group 'NSW01_LOCAL_NETS'
 set policy route NET-WIFI-VPN rule 5 set table 'main'
 
-set policy route NET-WIFI-VPN rule 10 description 'Route VLAN13 traffic to table 10'
+set policy route NET-WIFI-VPN rule 10 description 'Route VLAN10 traffic to table 10'
 set policy route NET-WIFI-VPN rule 10 set table '10'
-set policy route NET-WIFI-VPN rule 10 source address '10.120.13.0/24'
+set policy route NET-WIFI-VPN rule 10 source address '10.120.10.0/24'
 
-set policy route NET-WIFI-VPN rule 15 description 'Route VLAN10 traffic to table 10'
-set policy route NET-WIFI-VPN rule 15 set table '10'
-set policy route NET-WIFI-VPN rule 15 source address '10.120.10.0/24'
+set policy route NET-WIFI-VPN rule 20 description 'Route VLAN13 traffic to table 10'
+set policy route NET-WIFI-VPN rule 20 set table '10'
+set policy route NET-WIFI-VPN rule 20 source address '10.120.13.0/24'
+
+set policy route NET-WIFI-VPN rule 30 description 'Route VLAN14 traffic to table 10'
+set policy route NET-WIFI-VPN rule 30 set table '10'
+set policy route NET-WIFI-VPN rule 30 source address '10.120.14.0/24'
+
