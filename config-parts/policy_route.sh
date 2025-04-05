b3796524ffe5351281eb708e3ec9a3f8a0dfaf2e
Mark Hahl <mark.hahl@sigaint.au>
2025-04-05 14:19:17 +1100

    add interface to policy route

--- config-parts/policy_route.sh
+++ config-parts/policy_route.sh
@@ -1,6 +1,11 @@
 #/bin/vbash
 
-set policy route NET-WIFI-VPN interface 'br0.13'
+# Each interface must be added to the policy
+# or else it will not route correctly.
+#
+set policy route NET-WIFI-VPN interface 'br0.13' # WIFI
+set policy route NET-WIFI-VPN interface 'br0.14' # HOSTING
+
 set policy route NET-WIFI-VPN rule 5 description 'Bypass VPN for local networks'
 set policy route NET-WIFI-VPN rule 5 destination group network-group 'NSW01_LOCAL_NETS'
 set policy route NET-WIFI-VPN rule 5 set table 'main'
