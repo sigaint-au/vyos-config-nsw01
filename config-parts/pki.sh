cf4dc5b5fa65eb2bd69b28cb42d5ddc2ed826d8d
Mark Hahl <mark.hahl@sigaint.au>
2025-12-27 21:28:47 +1100

    fix commands

--- config-parts/pki.sh
+++ config-parts/pki.sh
@@ -3,5 +3,6 @@ set pki ca IPA system-install
 set pki ca IPA certificate "$(load_pem secrets/certificates/api/ca.crt)"
 
 # Certificates for API 
-set pki key-pair API private key "$(load_pem secrets/certificates/api/cert.key)"
-set pki key-pair API public key  "$(load_pem secrets/certificates/api/cert.pem)"
+set set certificate IPA description "Certificate for API"
+set pki certificate IPA private key "$(load_pem secrets/certificates/api/cert.key)"
+set set certificate IPA certificate  "$(load_pem secrets/certificates/api/cert.pem)"
