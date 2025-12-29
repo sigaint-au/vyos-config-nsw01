393780f7aaa5debec88a757c1cc9a51c7c56edd8
Mark Hahl <mark.hahl@sigaint.au>
2025-12-29 14:01:42 +1100

    feat: https api

--- config-parts/pki.sh
+++ config-parts/pki.sh
@@ -3,6 +3,6 @@ set pki ca IPA system-install
 set pki ca IPA certificate "$(load_pem secrets/certificates/api/ca.crt)"
 
 # Certificates for API 
-set pki certificate IPA description "Certificate for API"
-set pki certificate IPA private key "$(load_pem secrets/certificates/api/cert.key)"
-set pki certificate IPA certificate  "$(load_pem secrets/certificates/api/cert.pem)"
+set pki certificate IPA_API description "Certificate for API"
+set pki certificate IPA_API private key "$(load_pem secrets/certificates/api/cert.key)"
+set pki certificate IPA_API certificate  "$(load_pem secrets/certificates/api/cert.pem)"
